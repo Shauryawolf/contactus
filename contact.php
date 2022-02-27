459b959f2782e658a10d724b974bd9471fc1d7e2
Shauryawolf <89373027+Shauryawolf@users.noreply.github.com>
2022-02-27 10:04:01 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,16 +9,16 @@
 
     $email_subject = 'New form Submission';
 
-    $email_body = "Username : $name./n".
-                    "User Email : $visitor_email./n".
-                        "User Message : $message./n";
+    $email_body = "Username : $name.\n".
+                    "User Email : $visitor_email.\n".
+                        "User Message : $message.\n";
 
 
     $to = "shauryameena2021@gmail.com";
     
-    $headers = "From : $email_from /r/n";
+    $headers = "From : $email_from \r\n";
 
-    $headers .= "Reply-To: $visitor_email /r/n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
